5abaea25685dbb8171b4e087d87f9cc1a4802efa
Rajat Bhatnagar <rajatbhatnagar94@gmail.com>
2024-04-22 16:00:59 -0700

    add state

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-TIME_SLOTS=$(curl -s https://my.uscis.gov/appointmentscheduler-appointment/field-offices/state/WA | /usr/local/bin/jq '.[0].timeSlots | length')
+STATE="WA"
+TIME_SLOTS=$(curl -s https://my.uscis.gov/appointmentscheduler-appointment/field-offices/state/$STATE | /usr/local/bin/jq '.[0].timeSlots | length')
 
-echo "Num time slots: $TIME_SLOTS"
+echo "Num time slots: $TIME_SLOTS, STATE: $STATE"
 
 # check if time slot is available
 # publish to slack if available
 if [ $TIME_SLOTS -gt 0 ]; then
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Time slot available. Go to: https://my.uscis.gov/appointmentscheduler-appointment/ca/en/office-search"}' https://hooks.slack.com/services/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	
+	curl -X POST -H 'Content-type: application/json' --data '{"text":"Time slot available. Go to: https://my.uscis.gov/appointmentscheduler-appointment/ca/en/office-search"}' https://hooks.slack.com/services/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
 fi
